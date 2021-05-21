3336a4a910975639b762f9ec4288ad972d9ae90e
RigoGabriel <rigogabriel82@yahoo.com>
2021-05-21 11:48:10 +0200

    README

--- caesar_cipher/Decode_cesar.py
+++ caesar_cipher/Decode_cesar.py
@@ -3,18 +3,20 @@
 import sys
 
 def fonction_cesar(key, str):
+    key = 26 - key
     string = str.upper()
     res = ""
     for i in range(len(string)):
         char = string[i]
         if (char.isupper()):
             res += chr((ord(char) + key-65) % 26 + 65)
+    res = res.lower();
     return(res)
 
 lenSys = len(sys.argv)
 
 if lenSys > 3 or lenSys < 2:
-    print("Usage: ./cesar.py str key(key is optional)")
+    print("Usage: ./Decode_cesar.py str key(key is optional)")
 elif lenSys == 3:
     if sys.argv[2].isdigit():
         print fonction_cesar(int(sys.argv[2]) % 26, sys.argv[1])
